0f18749297c794ae537179d65181982130e3e2b5
deepredk <deepred.kim@gmail.com>
2022-03-09 18:34:52 +0900

    numbers를 가변 객체로 수정
    List.of()가 불변 객체를 반환해줘서 shuffle이 안됐던 거였음

--- src/kr/flab/NumberBaseball.java
+++ src/kr/flab/NumberBaseball.java
@@ -1,5 +1,6 @@
 package kr.flab;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +9,7 @@ public class NumberBaseball {
     private final char[] CORRECT_NUMBER_SET;
 
     public NumberBaseball() {
-        List<Character> numbers = List.of('1', '2', '3', '4', '5', '6', '7', '8', '9');
+        List<Character> numbers = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '9');
         Collections.shuffle(numbers);
 
         CORRECT_NUMBER_SET = new char[3];
